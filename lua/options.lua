9c02561dcfe41aecd2c5503e31bbf4217e5fc998
notken12 <kendotzhou@gmail.com>
2024-05-19 21:39:38 -0400

    add own stuff

--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,6 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt = 'both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 o.guifont = "MonaspiceXe Nerd Font Mono:h16"
+o.relativenumber = true
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -9,6 +9,8 @@ return {
 
   {
     "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
+    requires = { "williamboman/mason-lspconfig.nvim" },
     config = function()
       -- require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
@@ -16,8 +18,34 @@ return {
   },
 
   {
-    "mfussenegger/nvim-jdtls"
-  }, 
+    "mfussenegger/nvim-jdtls",
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    enabled = false,
+  },
+
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "BufEnter",
+    config = function()
+      require("nvim-highlight-colors").setup {
+        ---Render style
+        ---@usage 'background'|'foreground'|'virtual'
+        render = "virtual",
+
+        ---Set virtual symbol (requires render to be set to 'virtual')
+        virtual_symbol = "■",
+
+        ---Highlight named colors, e.g. 'green'
+        enable_named_colors = true,
+
+        ---Highlight tailwind colors, e.g. 'bg-blue-500'
+        enable_tailwind = true,
+      }
+    end,
+  },
 
   -- {
   -- 	"williamboman/mason.nvim",
